3015ed78203f2425ad998c775ceae789faf26bae
impiyush83 <nalawadepiyush@gmail.com>
2023-03-08 14:18:26 -0500

    fix: error handling

--- app.js
+++ app.js
@@ -18,17 +18,25 @@ app.use(requestParser());
 app.use('/v1', routes);
 
 app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+    res.send(404).json({'message': 'Service Up & Running'});
 });
 
 app.get('/', (req, res) => {
-    res.send(200, 'Ok');
+    res.send(200).json({'message': 'OK'});
 });
 
 app.listen(port, () => {
     logger.info(`Example app listening on port ${port}`);
 });
 
-app.use((err, req, res) => {
+app.use((req, res, next) => {
+    res.send(404).json({'message': 'Invalid Route'});
+});
+
+// error handler
+// The `next` var needs to be left below to ensure express picks this function as error handler
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+    logger.error('Error caught by error handler: ', err);
     responseHandler.handleError(err, res);
 });
